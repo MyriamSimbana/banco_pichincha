fd012136ad29bcb3c05317b27e0e0be189d55315
MyriamSimbana <miasimbanac@hotmail.com>
2022-06-15 21:21:57 -0500

    masc: first commit

---
+++ src/test/java/com/evaluation/q2/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluation.q2.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
